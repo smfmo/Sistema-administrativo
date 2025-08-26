f005cc363ea474414d08bcffc12a558e5afda165
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-26 17:01:48 -0300

    .

--- src/main/java/com/samuel/contratos/controller/AdminController.java
+++ src/main/java/com/samuel/contratos/controller/AdminController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-    //‘Login’ do administrador
     @GetMapping("/loginAdm")
     public String loginAdm(){
         return "loginAdm";
