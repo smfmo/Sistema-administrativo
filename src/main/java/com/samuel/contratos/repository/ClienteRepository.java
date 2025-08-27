7f271c559c829b9f05463f58150ad2056b27b374
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-27 09:02:37 -0300

    melhora na nomenclatura do nome da interface

--- src/main/java/com/samuel/contratos/repository/ClienteRepository.java
+++ src/main/java/com/samuel/contratos/repository/ClienteRepository.java
@@ -3,11 +3,10 @@ package com.samuel.contratos.repository;
 import com.samuel.contratos.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 import java.util.UUID;
 
-public interface ClientesRepository extends JpaRepository<Cliente, UUID> {
+public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
 
     List<Cliente> findByNomeContainingIgnoreCase(String nome);
 
