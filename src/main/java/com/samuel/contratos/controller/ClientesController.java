fc83041c910b9838318fc64f3850fc9171cd9b89
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-08 17:02:04 -0300

    refactor (api): Ajustar nomes de dependências e dtos conforme as mudanças

--- src/main/java/com/samuel/contratos/controller/ClientesController.java
+++ src/main/java/com/samuel/contratos/controller/ClientesController.java
@@ -1,9 +1,8 @@
 package com.samuel.contratos.controller;
 
-import com.samuel.contratos.controller.dtos.ClienteDto;
+import com.samuel.contratos.controller.dtos.request.ClienteRequestDTO;
 import com.samuel.contratos.controller.mappers.ClienteMapper;
 import com.samuel.contratos.model.Cliente;
-import com.samuel.contratos.model.Contrato;
 import com.samuel.contratos.service.ClienteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,56 +18,55 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ClientesController {
 
-    private final ClienteService clienteService;
-    private final ClienteMapper clienteMapper;
+    private final ClienteService service;
+    private final ClienteMapper mapper;
 
     @GetMapping
-    public String getCustomerForm(@ModelAttribute ClienteDto clienteDto,
+    public String getCustomerForm(@ModelAttribute ClienteRequestDTO request,
                                   Model model) {
-        model.addAttribute("clienteDto", clienteMapper.toEntity(clienteDto));
+        model.addAttribute("clienteRequest", mapper.toEntity(request));
         return "formulario-cliente";
     }
 
     @PostMapping
-    public String save(@ModelAttribute("clienteDto") @Valid ClienteDto clienteDto,
+    public String save(@ModelAttribute("clienteDto") @Valid ClienteRequestDTO request,
                                 BindingResult bindingResult) {
         if (bindingResult.hasErrors()) { //se houver erros no formulário, volta para o mesmo
             return "formulario-cliente";
         }
-        clienteService.save(clienteDto);
+        service.save(request);
         return "redirect:/inicio";
     }
 
     @GetMapping("/editar/{id}")
     public String partialUpdate(@PathVariable("id") UUID id,
-                                @ModelAttribute("clienteDto") @Valid ClienteDto clienteDto) {
-        clienteService.partialUpdate(id, clienteDto);
+                                @ModelAttribute("clienteRequest") @Valid ClienteRequestDTO request) {
+        service.partialUpdate(id, request);
         return "/Atualizar-cliente";
     }
 
     @GetMapping("/list")
     public String listarClientes(Model model) {
-        List<Cliente> clientes = clienteService.listarClientes();
+        List<Cliente> clientes = service.listarClientes();
         model.addAttribute("clientes", clientes);
 
         return "Controle-de-clientes";
     }
 
     @GetMapping("/search")
-    public String pesquisarCliente(@RequestParam(name = "nome",
-                                   required = false) String nome,
+    public String pesquisarCliente(@RequestParam(name = "nome", required = false) String nome,
                                    Model model) {
 
-        List<Cliente> resultado = clienteService.pesquisarCliente(nome);
+        List<Cliente> resultado = service.pesquisarCliente(nome);
         model.addAttribute("clientes", resultado);
 
         return "Controle-de-clientes";
     }
 
     @GetMapping("/{id}")
-    public String getById(@PathVariable("id") UUID id,
-                          Model model) {
-       Cliente cliente = clienteService.informacoesCliente(id);
+    public String getById(@PathVariable("id") UUID id, Model model) {
+
+       Cliente cliente = service.informacoesCliente(id);
        model.addAttribute("cliente", cliente);
        return "informacoes-cliente";
     }
