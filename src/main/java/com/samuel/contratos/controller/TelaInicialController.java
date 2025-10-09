b6c0d3a1a26d9ad2c0b6d8557d5ec05af13eb79f
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-09 12:20:45 -0300

    refactor (controller): extrair modelAttribute de pegar o usuário logado para o controllerAdvice de modelAttribute global

--- src/main/java/com/samuel/contratos/controller/TelaInicialController.java
+++ src/main/java/com/samuel/contratos/controller/TelaInicialController.java
@@ -1,12 +1,10 @@
 package com.samuel.contratos.controller;
 
-import com.samuel.contratos.model.UserAdm;
 import com.samuel.contratos.service.ClienteService;
 import com.samuel.contratos.service.ContratoService;
 import com.samuel.contratos.service.GraficStatisticsService;
 import com.samuel.contratos.service.GraficsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +21,7 @@ public class TelaInicialController {
     private final GraficStatisticsService graficsStatisticsService;
 
     @GetMapping
-    public String index(Model model,
-                        @AuthenticationPrincipal UserAdm user) {
-        model.addAttribute("user", user); //busca o usuário logado.
+    public String index(Model model) {
 
         Long totalContratos = contratoService.totalContratos();
         Long totalClientes = clienteService.contagemDeClientes();
@@ -36,15 +32,6 @@ public class TelaInicialController {
         model.addAttribute("totais", graficsService.mostrarGrafico());
         model.addAttribute("meses", graficsStatisticsService.gerarMesesNoIntervalo());
 
-
         return "inicio";
     }
-
-/*    @GetMapping("controle-contratos")
-//    public String ExibirTodosContratos(Model model) {
-//        model.addAttribute("contratos",
-//                contratoService.buscarTodosOsContratos());
-//
-//        return "controle-contratos";
- }*/
 }
