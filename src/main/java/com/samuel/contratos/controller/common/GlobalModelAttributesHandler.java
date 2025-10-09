6f918772017105ba29737084244d273056500571
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-09 12:08:16 -0300

    feat (controller): Implementar Controlleradvice para centralização dos modelAttributes globais da aplicação

---
+++ src/main/java/com/samuel/contratos/controller/common/GlobalModelAttributesHandler.java
@@ -0,0 +1,35 @@
+package com.samuel.contratos.controller.common;
+
+import com.samuel.contratos.model.Cliente;
+import com.samuel.contratos.model.UserAdm;
+import com.samuel.contratos.service.ClienteService;
+import com.samuel.contratos.service.ContratoService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalModelAttributesHandler {
+
+    private final ClienteService clienteService;
+    private final ContratoService contratoService;
+
+
+    public GlobalModelAttributesHandler(ClienteService clienteService,
+                                        ContratoService contratoService) {
+        this.clienteService = clienteService;
+        this.contratoService = contratoService;
+    }
+
+    @ModelAttribute("listaClientes")
+    public List<Cliente> findAllClientes() {
+        return clienteService.listarClientes();
+    }
+
+    @ModelAttribute("user")
+    public UserAdm getUser(@AuthenticationPrincipal UserAdm user) {
+        return user;
+    }
+
+}
