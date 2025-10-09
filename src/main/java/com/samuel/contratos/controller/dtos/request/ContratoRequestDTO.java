84ba491b95fcc5e08452febc89b094569db6f2b9
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-09 16:28:20 -0300

    refactor (controller): Aplicar mudanças no Array de arquivos para lista

--- src/main/java/com/samuel/contratos/controller/dtos/request/ContratoRequestDTO.java
+++ src/main/java/com/samuel/contratos/controller/dtos/request/ContratoRequestDTO.java
@@ -2,11 +2,16 @@ package com.samuel.contratos.controller.dtos.request;
 
 import com.samuel.contratos.model.Cliente;
 import com.samuel.contratos.model.Enum.TiposDeContrato;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record ContratoRequestDTO(
+        @NotNull(message = "Agência não pode ser nulo")
+        @Size(max = 4)
         String agencia,
+        @NotNull(message = "Sr não pode ser nulo")
         String sr,
         String valorBruto,
         String valorLiquido,
