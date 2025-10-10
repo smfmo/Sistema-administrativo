4721fd21f9aef8061ab98fa26677ce6f2eadb2ea
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-10 12:26:10 -0300

    feat (dto): Implementar mascaras no dto para conversão dos tipos monetários antes de persistir no banco

--- src/main/java/com/samuel/contratos/controller/dtos/request/ContratoRequestDTO.java
+++ src/main/java/com/samuel/contratos/controller/dtos/request/ContratoRequestDTO.java
@@ -3,8 +3,11 @@ package com.samuel.contratos.controller.dtos.request;
 import com.samuel.contratos.model.Cliente;
 import com.samuel.contratos.model.Enum.TiposDeContrato;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
+import org.springframework.format.annotation.NumberFormat;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public record ContratoRequestDTO(
@@ -13,13 +16,20 @@ public record ContratoRequestDTO(
         String agencia,
         @NotNull(message = "Sr não pode ser nulo")
         String sr,
-        String valorBruto,
-        String valorLiquido,
-        String prestacao,
+        @NumberFormat(pattern = "#,##0.00", style = NumberFormat.Style.NUMBER)
+        BigDecimal valorBruto,
+        @NumberFormat(pattern = "#,##0.00", style = NumberFormat.Style.NUMBER)
+        BigDecimal valorLiquido,
+        @NumberFormat(pattern = "#,##0.00", style = NumberFormat.Style.NUMBER)
+        BigDecimal prestacao,
         String parcelas,
-        String prestamista,
-        String iof,
-        String jurosAcerto,
+        @NumberFormat(pattern = "#,##0.00", style = NumberFormat.Style.NUMBER)
+        BigDecimal prestamista,
+        @NumberFormat(pattern = "#,##0.00", style = NumberFormat.Style.NUMBER)
+        BigDecimal iof,
+        @NumberFormat(pattern = "#,##0.00", style = NumberFormat.Style.NUMBER)
+        BigDecimal jurosAcerto,
+        @Past(message = "Data da contratação não pode ser uma data futura")
         LocalDate data,
         Cliente cliente,
         TiposDeContrato tipoContrato,
