55a8c9eac72d7f84dcbf4665a7d92ea0ce057964
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-13 17:04:32 -0300

    refactor (persistence): Renomear repositório de UserRepository para EmployeeRepository

--- src/main/java/com/samuel/contratos/repository/EmployeeRepository.java
+++ src/main/java/com/samuel/contratos/repository/EmployeeRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<Employee, UUID> {
+public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
     Optional<Employee> findByUsername(String username);
 
 }
