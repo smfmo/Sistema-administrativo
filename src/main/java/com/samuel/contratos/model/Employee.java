37532c615a3b57543855dfc1646d7a1a1c36b037
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-13 17:03:40 -0300

    refactor (model): Renomear classe User para Employee
    - Renomeia classe User para Employee para melhor localidade dentro do sistema.
    - Remove implementação de UserDetails aonde será utilizada em um serviço a parte.

--- src/main/java/com/samuel/contratos/model/Employee.java
+++ src/main/java/com/samuel/contratos/model/Employee.java
@@ -2,17 +2,14 @@ package com.samuel.contratos.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import java.util.Collection;
-import java.util.Collections;
+
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "employee",
         schema = "public")
-public class User {
+public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
