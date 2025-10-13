012ce41ffae9ca26cc973e049a37c7495c4ca991
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-13 17:00:05 -0300

    refactor (main): Renomear classe principal apenas para Application

--- src/main/java/com/samuel/contratos/Application.java
+++ src/main/java/com/samuel/contratos/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContratosApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContratosApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		System.out.println("Contratos Application Started");
 	}
 
