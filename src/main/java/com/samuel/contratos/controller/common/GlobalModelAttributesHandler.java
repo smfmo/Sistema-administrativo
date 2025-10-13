3d4a44b58decb9bcdf084f20e346b1f8b309a052
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-13 17:24:24 -0300

    refactor: Ajustar importações

--- src/main/java/com/samuel/contratos/controller/common/GlobalModelAttributesHandler.java
+++ src/main/java/com/samuel/contratos/controller/common/GlobalModelAttributesHandler.java
@@ -1,10 +1,10 @@
 package com.samuel.contratos.controller.common;
 
 import com.samuel.contratos.model.Cliente;
-import com.samuel.contratos.model.User;
 import com.samuel.contratos.service.ClienteService;
 import com.samuel.contratos.service.ContratoService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import java.util.List;
