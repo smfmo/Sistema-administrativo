64cc747b0caa506ccb46af18cb43b76ec98031fd
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-15 13:01:54 -0300

    feat (service): Injetar SecurityService e setar o usuário logado para a auditoria no momento em que for persistir o cliente no banco

--- src/main/java/com/samuel/contratos/service/ClienteService.java
+++ src/main/java/com/samuel/contratos/service/ClienteService.java
@@ -4,6 +4,7 @@ import com.samuel.contratos.controller.dtos.request.ClienteRequestDTO;
 import com.samuel.contratos.controller.mappers.ClienteMapper;
 import com.samuel.contratos.model.Cliente;
 import com.samuel.contratos.repository.ClienteRepository;
+import com.samuel.contratos.security.SecurityService;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,14 +16,18 @@ public class ClienteService {
 
     private final ClienteRepository repository;
     private final ClienteMapper mapper;
+    private final SecurityService securityService;
 
-    public ClienteService(ClienteRepository repository, ClienteMapper mapper) {
+    public ClienteService(ClienteRepository repository, ClienteMapper mapper,
+                          SecurityService securityService) {
         this.repository = repository;
         this.mapper = mapper;
+        this.securityService = securityService;
     }
 
     @Transactional
     public void save(Cliente cliente) {
+        cliente.setEmployee(securityService.getEmployeeAuthenticated());
         repository.save(cliente);
     }
 
