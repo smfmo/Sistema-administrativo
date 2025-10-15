7bf43f6634edc887ed1f3b8f19a76cb6a210df88
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-15 12:56:39 -0300

    feat (Auditing): Anotar Classe principal da aplicação com @EnableJpaAuditing para adicionar as auditorias automáticas

--- src/main/java/com/samuel/contratos/Application.java
+++ src/main/java/com/samuel/contratos/Application.java
@@ -2,14 +2,14 @@ package com.samuel.contratos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Contratos Application Started");
 	}
-
-
 }
