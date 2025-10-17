b122a322159f80dd7814dcb41ad8ad268fcb9504
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-10-17 13:22:12 -0300

    feat (persistence): Mudar método de buscar por nome de cliente de List para Page, para adicionar lógica de Paginação

--- src/main/java/com/samuel/contratos/repository/ClienteRepository.java
+++ src/main/java/com/samuel/contratos/repository/ClienteRepository.java
@@ -1,14 +1,18 @@
 package com.samuel.contratos.repository;
 
 import com.samuel.contratos.model.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import java.util.List;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
 
-    List<Cliente> findByNomeContainingIgnoreCase(String nome);
+    Page<Cliente> findByNomeContainingIgnoreCase(@Param("nome") String nome, Pageable pageable);
 
     @Query("SELECT COUNT(c) FROM Cliente c")
     Long countTotalClientes();
